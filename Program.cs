bbd1461591a96eee9f29f764aaab568e61432d99
Caroline Arena <caroline.arena9333@gmail.com>
2025-05-28 15:11:53 -0300

    Adicionando estrutura inicial do projeto POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("1199998888", "Ninguem Usa +", "123456", 128);
+
+        Smartphone iphone = new Iphone("1199998888", "16 Pro", "12345", 128);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Facebook");
+        Console.WriteLine();
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+        Console.WriteLine();
+    }
+}
